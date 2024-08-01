74e438a324be8d5a6456c498e1684827cabf3920
Ramon Chaves <144055828+Mushytu@users.noreply.github.com>
2024-08-01 15:37:20 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/mushytu/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.mushytu.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mushytu.workshopmongo.domain.User;
+import com.mushytu.workshopmongo.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
